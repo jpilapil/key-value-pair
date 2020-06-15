4e9a4c96f364a0f1237f039888eec1ceec66cdfe
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-15 09:33:02 -0700

    add other user ui

--- src/components/pages/Connect.js
+++ src/components/pages/Connect.js
@@ -1,14 +1,20 @@
 import React from "react";
 import AppTemplate from "../ui/AppTemplate";
+import OtherUser from "../ui/OtherUser";
 // import { Link } from "react-router-dom";
 
 export default function Connect() {
   return (
     <AppTemplate>
       <div>
-        <div className="mt-9">
-          <div className="card card-body">
-            <h2 className="text-light">OTHER USER</h2>
+        <div className=" mt-9">
+          <div className="row">
+            <OtherUser />
+            <OtherUser />
+            <OtherUser />
+            <OtherUser />
+            <OtherUser />
+            <OtherUser />
           </div>
         </div>
       </div>
---
+++ src/components/ui/OtherUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function OtherUser() {
+  return (
+    <div className="col-6 my-3">
+      <div className="card card-body">
+        <h2 className="text-secondary">OTHER USER</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit
+          amet elementum turpis. Duis eu quam magna. Praesent facilisis eleifend
+          orci, in commodo sem blandit bibendum. In eu feugiat nisl, non
+          dignissim neque. Nullam scelerisque congue placerat. Pellentesque sit
+          amet efficitur eros. Pellentesque odio lectus, eleifend nec sem ut,
+          tincidunt.
+        </p>
+      </div>
+    </div>
+  );
+}
